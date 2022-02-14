05af5f901203ec78aec37df4f9a87d2e44e9df03
Taweechai Yuenyang <krumii.it@gmail.com>
2022-02-14 09:46:48 +0700

    เปลี่ยนเส้นทาง

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
     //     'laravelVersion' => Application::VERSION,
     //     'phpVersion' => PHP_VERSION,
     // ]);
-    return redirect('/dashboard');
+    return redirect('dashboard');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
